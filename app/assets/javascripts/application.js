578145d321edf256cc65a31165c6b3a99e83782f
Adrian Maseda <amaseda@gmail.com>
2016-04-04 11:17:03 -0400

    can check if date avail or not. form only submits if avail.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,29 +15,35 @@
 //= require_tree .
 
 $(function(){
-  var $createEventButton = $('.js-is-not-available')
+  var $createEventButton = $('.js-submit-event')
+  var $formNewEvent = $("form.new_event")
   $(".datetimepicker").datetimepicker();
-  $createEventButton.hide();
-  $(".js-check-availability").on("click", function(e){
+  $formNewEvent.on("submit", function(e){
+    if( $formNewEvent.hasClass("js-is-available") ){
+      return true;
+    }
     e.preventDefault();
     var spaceId = $("[name='event[space_id]']").val()
     var start_date = $("[name='event[start_date]']").val()
     var end_date = $("[name='event[end_date]']").val()
     console.log(spaceId, start_date, end_date)
-    var url = "/events/check_availability?space_id=" + spaceId + "&start_date=" + start_date + "&end_date=" + end_date; 
-    $(this).addClass('loading')
+    var url = "/events/check_availability?space_id=" + spaceId + "&start_date=" + start_date + "&end_date=" + end_date;
     $.getJSON(url, function(response){
       console.log(response)
       if(response === true){
-	$createEventButton.show();
+        $createEventButton.val("Create Event");
+        $formNewEvent.addClass("js-is-available");
       } else {
-	$createEventButton.hide();
+	      $createEventButton.val("Not Available");
+        setTimeout( function(){
+          $createEventButton.val("Check Availability");
+        }, 2000)
       }
     })
   })
 
   $("input[type='color']").on("change", function(e){
-    console.log($(e.target).val()) 
+    console.log($(e.target).val())
     $color = $(e.target)
     var id = $color.attr('id')
     var color = $color.val()
@@ -48,5 +54,3 @@ $(function(){
     })
   })
 })
-
-
