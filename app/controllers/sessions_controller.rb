5a59dbe04d5d5af4a58ca14dd637f06113c74462
Jesse Shawl <jesse@jshawl.com>
2016-04-14 11:11:29 -0400

    restrict login to ga emails only

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,10 @@
 class SessionsController < ApplicationController
   def create
     auth = request.env["omniauth.auth"]
+    unless auth["info"]["email"].match(/generalassemb\.ly/)
+      flash[:notice] = "Must login with a ga email address"
+      return redirect_to root_url
+    end
     user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth)
     session[:user_id] = user.id
     redirect_to root_url
