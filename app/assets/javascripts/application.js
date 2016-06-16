3206c892bae2696f14bf29c87fe6e2a3305fe4a2
Jesse Shawl <jesse@jshawl.com>
2016-06-16 09:15:57 -0400

    remove validation

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,32 +17,8 @@
 //= require_tree .
 
 $(function(){
-  var $createEventButton = $('.js-submit-event')
-  var $formNewEvent = $("form.new_event")
   $(".datetimepicker").datetimepicker();
-  $formNewEvent.on("submit", function(e){
-    if( $formNewEvent.hasClass("js-is-available") ){
-      return true;
-    }
-    e.preventDefault();
-    var spaceId = $("[name='event[space_id]']").val()
-    var start_date = $("[name='event[start_date]']").val()
-    var end_date = $("[name='event[end_date]']").val()
-    console.log(spaceId, start_date, end_date)
-    var url = "/events/check_availability?space_id=" + spaceId + "&start_date=" + start_date + "&end_date=" + end_date;
-    $.getJSON(url, function(response){
-      console.log(response)
-      if(response === true){
-        $createEventButton.val("Create Event");
-        $formNewEvent.addClass("js-is-available");
-      } else {
-	      $createEventButton.val("Not Available");
-        setTimeout( function(){
-          $createEventButton.val("Check Availability");
-        }, 2000)
-      }
-    })
-  })
+})
 
   $("input[type='color']").on("change", function(e){
     console.log($(e.target).val())
