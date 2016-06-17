f28fbdb9b1bd4aee641689016ae5677bf30cad25
Andy <andrew.sunglae.kim@gmail.com>
2016-06-17 12:28:46 -0400

    ajax sends correct parameters

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,12 @@ Bundler.require(*Rails.groups)
 
 module MasterPlan
   class Application < Rails::Application
+    config.middleware.insert_before 0, "Rack::Cors" do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :options]
+      end
+    end
     # Settings in config/environments/* take precedence over those specified here.
     config.serve_static_files=true
     # Application configuration should go into files in config/initializers
