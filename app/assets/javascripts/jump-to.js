42c22f5bc0bef02a8e4856fd6b6254ea99a00e54
Jesse Shawl <jesse@jshawl.com>
2016-06-22 16:56:59 -0400

    prevent invalid date bug

--- app/assets/javascripts/jump-to.js
+++ app/assets/javascripts/jump-to.js
@@ -2,6 +2,7 @@ $(function(){
   $(".js-jump-to").datetimepicker({
     timepicker: false,
     onChangeDateTime: function(){
+      if( !this.getValue() ) return
       window.location = "/days/" + moment(this.getValue()).format("YYYY-MM-DD") + window.location.search
     }
   })
