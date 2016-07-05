da251338e841b209bfef1b71b8b3e498e20444d8
Jesse Shawl <jesse@jshawl.com>
2016-07-05 15:04:21 -0400

    fixes #87

--- app/assets/javascripts/events-new.js
+++ app/assets/javascripts/events-new.js
@@ -1,8 +1,6 @@
 $(function(){
-
   var $input = $("[name='event[custom_color]']")
   var $dropdown = $("[name='event[event_type_id]']")
-
   if($input.val() === "#000000"){
     updateCustomColor()
   }
@@ -13,5 +11,9 @@ $(function(){
     var newColor = colors[selected]
     $input.val(newColor)
   }
-
+  $startDate = $('[name="event[start_date]"]')
+  $endDate = $('[name="event[end_date]"]')
+  $startDate.on("change", function(){
+    $endDate.val($(this).val())
+  })
 })
