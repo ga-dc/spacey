18008ed64570f0fb34883743e7d60e127dc5b9e5
Jesse Shawl <jesse@jshawl.com>
2016-07-05 14:49:30 -0400

    fixes #86

--- app/assets/javascripts/jump-to.js
+++ app/assets/javascripts/jump-to.js
@@ -1,9 +1,15 @@
 $(function(){
-  $(".js-jump-to").datetimepicker({
+
+  var opts = {
     timepicker: false,
     onChangeDateTime: function(){
       if( !this.getValue() ) return
       window.location = "/days/" + moment(this.getValue()).format("YYYY-MM-DD") + window.location.search
     }
-  })
+  }
+  var dateInUrl = window.location.pathname.match(/[0-9]{4}-[0-9]{2}-[0-9]{2}/)
+  if(dateInUrl){
+    opts.defaultDate = dateInUrl[0] 
+  }
+  $(".js-jump-to").datetimepicker(opts)
 })
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160627202130) do
+ActiveRecord::Schema.define(version: 20160701132856) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,12 @@ ActiveRecord::Schema.define(version: 20160627202130) do
   end
 
   add_index "notes", ["event_id"], name: "index_notes_on_event_id", using: :btree
+
+  create_table "place_cards", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "recurring_events", force: :cascade do |t|
     t.json     "recurring_rules"
